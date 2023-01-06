940d2075535eab3f1b17bf15aa84922393a11c2e
fayzar ali <fayzarmahmud00@gmail.com>
2023-01-06 20:25:06 +0600

    dom2 project clone again taday

---
+++ dom2_clone/index.js
@@ -0,0 +1,68 @@
+// for (let i = 0; i < 3; i++) {
+//   let playBtn = document.querySelectorAll(".myButton")[i];
+//   playBtn.addEventListener("click", function () {
+//     let text = this.innerHTML;
+//     console.log(text);
+//     playAudion(text);
+//   });
+// }
+
+// function playAudion(text) {
+//   switch (text) {
+//     case "a":
+//       var audio = new Audio("sounds/a1.mp3");
+//       audio.play();
+//       break;
+
+//     case "b":
+//       var audio = new Audio("sounds/a2.mp3");
+//       audio.play();
+//       break;
+
+//     case "c":
+//       var audio = new Audio("sounds/a3.mp3");
+//       audio.play();
+//       break;
+//   }
+// }
+
+for (let i = 0; i < 3; i++) {
+  let selectButton = document.querySelectorAll(".myButton")[i];
+  selectButton.addEventListener("click", function () {
+    let butonText = this.innerHTML;
+    console.log(butonText);
+    playAudio(butonText);
+    playAnimation(butonText);
+  });
+}
+// creating a audio play function
+
+function playAudio(butonText) {
+  switch (butonText) {
+    case "a":
+      var audio = new Audio("sounds/a1.mp3");
+      audio.play();
+      break;
+
+    case "b":
+      var audio = new Audio("sounds/a2.mp3");
+      audio.play();
+      break;
+
+    case "c":
+      var audio = new Audio("sounds/a3.mp3");
+      audio.play();
+      break;
+  }
+}
+
+//creating a function add animation using js
+
+function playAnimation(butonText) {
+  let animBtn = document.querySelector("." + butonText);
+  animBtn.classList.add("animation");
+
+  setTimeout(function () {
+    animBtn.classList.remove("animation");
+  }, 1000);
+}
